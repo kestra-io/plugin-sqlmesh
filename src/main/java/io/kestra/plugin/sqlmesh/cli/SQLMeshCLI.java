d54b78c919d01ce360ecd0d2c59302d50ba67917
iNikitaGricenko <VfVristalt@gmail.com>
2023-10-31 14:14:07 +0200

    Implement SQLMeshCLI

---
+++ src/main/java/io/kestra/plugin/sqlmesh/cli/SQLMeshCLI.java
@@ -0,0 +1,117 @@
+package io.kestra.plugin.sqlmesh.cli;
+
+import io.kestra.core.models.annotations.Example;
+import io.kestra.core.models.annotations.Plugin;
+import io.kestra.plugin.scripts.exec.scripts.models.DockerOptions;
+import io.kestra.plugin.scripts.exec.scripts.models.RunnerType;
+import io.kestra.plugin.scripts.exec.scripts.models.ScriptOutput;
+import io.kestra.plugin.scripts.exec.scripts.runners.CommandsWrapper;
+import io.kestra.plugin.scripts.exec.scripts.services.ScriptService;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import org.apache.commons.lang3.StringUtils;
+import io.kestra.core.models.annotations.PluginProperty;
+import io.kestra.core.models.tasks.RunnableTask;
+import io.kestra.core.models.tasks.Task;
+import io.kestra.core.runners.RunContext;
+import org.slf4j.Logger;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import static io.kestra.core.utils.Rethrow.throwFunction;
+
+@SuperBuilder
+@ToString
+@EqualsAndHashCode
+@Getter
+@NoArgsConstructor
+@Schema(
+    title = "Execute sqlmesh command"
+)
+@Plugin(
+    examples = {
+        @Example(
+            title = "Initialize duckdb in sqlmesh",
+            code = {
+                """
+                id: transform
+                namespace: dev
+                tasks:
+                  - id: sqlmesh
+                    type: io.kestra.plugin.sqlmesh.cli.SQLMeshCLI
+                    commands:
+                      - sqlmesh init duckdb
+                """
+            }
+        )
+    }
+)
+public class SQLMeshCLI extends Task implements RunnableTask<ScriptOutput> {
+    private static final String DEFAULT_IMAGE = "ghcr.io/kestra-io/sqlmesh";
+
+    @Schema(
+        title = "The commands to run before main list of commands"
+    )
+    @PluginProperty(dynamic = true)
+    protected List<String> beforeCommands;
+
+    @Schema(
+        title = "The commands to run"
+    )
+    @PluginProperty(dynamic = true)
+    @NotNull
+    @NotEmpty
+    protected List<String> commands;
+
+    @Schema(
+        title = "Additional environment variables for the current process."
+    )
+    @PluginProperty(
+        additionalProperties = String.class,
+        dynamic = true
+    )
+    protected Map<String, String> env;
+
+    @Schema(
+        title = "Docker options when for the `DOCKER` runner",
+        defaultValue = "{image=" + DEFAULT_IMAGE + ", pullPolicy=ALWAYS}"
+    )
+    @PluginProperty
+    @Builder.Default
+    protected DockerOptions docker = DockerOptions.builder().build();
+
+    @Override
+    public ScriptOutput run(RunContext runContext) throws Exception {
+        return new CommandsWrapper(runContext)
+            .withWarningOnStdErr(true)
+            .withRunnerType(RunnerType.DOCKER)
+            .withDockerOptions(injectDefaults(getDocker()))
+            .withEnv(Optional.ofNullable(env).orElse(new HashMap<>()))
+            .withCommands(
+                ScriptService.scriptCommands(
+                    List.of("/bin/sh", "-c"),
+                    Optional.ofNullable(this.beforeCommands).map(throwFunction(runContext::render)).orElse(null),
+                    runContext.render(this.commands)
+                                            )
+                         )
+            .run();
+    }
+
+    private DockerOptions injectDefaults(DockerOptions original) {
+        var builder = original.toBuilder();
+        if (original.getImage() == null) {
+            builder.image(DEFAULT_IMAGE);
+        }
+        if (original.getEntryPoint() == null || original.getEntryPoint().isEmpty()) {
+            builder.entryPoint(List.of(""));
+        }
+
+        return builder.build();
+    }
+}
---
+++ src/main/java/io/kestra/plugin/sqlmesh/cli/package-info.java
@@ -0,0 +1,7 @@
+@PluginSubGroup(
+    description = "This sub-group of plugins contains tasks for using SQLMesh.\n",
+    categories = PluginSubGroup.PluginCategory.TOOL
+)
+package io.kestra.plugin.sqlmesh.cli;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
