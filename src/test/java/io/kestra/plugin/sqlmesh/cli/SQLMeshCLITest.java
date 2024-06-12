2a2745106b2f9333bdc1abfed06fa2d5015f9d43
brian.mulier <bmmulier@hotmail.fr>
2024-06-12 19:50:54 +0200

    fix(tests): introduce @KestraTest (linked to kestra-io/kestra#3948)

--- src/test/java/io/kestra/plugin/sqlmesh/cli/SQLMeshCLITest.java
+++ src/test/java/io/kestra/plugin/sqlmesh/cli/SQLMeshCLITest.java
@@ -6,7 +6,7 @@ import io.kestra.core.utils.TestsUtils;
 import io.kestra.plugin.scripts.exec.scripts.models.DockerOptions;
 import io.kestra.plugin.scripts.exec.scripts.models.ScriptOutput;
 import io.kestra.plugin.sqlmesh.cli.SQLMeshCLI;
-import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import io.kestra.core.junit.annotations.KestraTest;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.Test;
 import io.kestra.core.runners.RunContext;
@@ -21,7 +21,7 @@ import java.util.Map;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
-@MicronautTest
+@KestraTest
 class SQLMeshCLITest {
 
     @Inject
