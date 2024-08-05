360cc0a3edd6eaa2e4c91f97057b7e27763bea30
Loïc Mathieu <loikeseke@gmail.com>
2024-08-05 19:17:09 +0200

    fix: task runner INSTANCE -> instance()

--- src/main/java/io/kestra/plugin/sqlmesh/cli/SQLMeshCLI.java
+++ src/main/java/io/kestra/plugin/sqlmesh/cli/SQLMeshCLI.java
@@ -94,7 +94,7 @@ public class SQLMeshCLI extends Task implements RunnableTask<ScriptOutput>, Name
     @PluginProperty
     @Builder.Default
     @Valid
-    private TaskRunner taskRunner = Docker.INSTANCE;
+    private TaskRunner taskRunner = Docker.instance();
 
     @Schema(title = "The task runner container image, only used if the task runner is container-based.")
     @PluginProperty(dynamic = true)
