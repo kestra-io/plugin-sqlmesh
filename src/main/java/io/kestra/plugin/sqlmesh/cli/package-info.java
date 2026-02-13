aea26ee7f1565906a79fe7fc1e0bd73ee504b731
François Delbrayelle <fdelbrayelle@gmail.com>
2026-02-13 08:53:35 +0100

    chore: update plugin categories (#49)

--- src/main/java/io/kestra/plugin/sqlmesh/cli/package-info.java
+++ src/main/java/io/kestra/plugin/sqlmesh/cli/package-info.java
@@ -1,7 +1,8 @@
 @PluginSubGroup(
     title = "SQLMesh CLI",
-    description = "This sub-group of plugins contains tasks for using SQLMesh.\n",
-    categories = PluginSubGroup.PluginCategory.TOOL
+    description = "This sub-group of plugins contains tasks for using SQLMesh.\n",categories = {
+        PluginSubGroup.PluginCategory.DATA
+    }
 )
 package io.kestra.plugin.sqlmesh.cli;
 
